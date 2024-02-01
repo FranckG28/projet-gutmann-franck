9d423abef18b0a37ebf3b0592306b74ef183737b
Franck <gutmann.franck@outlook.fr>
2024-02-01 15:20:08 +0100

    build: install taigaUI, TailwindCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER, TuiButtonModule } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuiIslandModule } from "@taiga-ui/kit";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiIslandModule, TuiButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }]
 })
 export class AppComponent {
   title = 'livl-store';
