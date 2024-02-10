43ce91bc4fbf3e30f1b93f0a491c75333a868b86
Franck <gutmann.franck@outlook.fr>
2024-02-10 16:53:28 +0100

    git status

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { provideAnimations } from "@angular/platform-browser/animations";
-import { TuiRootModule } from "@taiga-ui/core";
+import { TUI_SANITIZER, TuiRootModule } from "@taiga-ui/core";
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { TUI_FRENCH_LANGUAGE, TUI_LANGUAGE } from "@taiga-ui/i18n";
 import { of } from "rxjs";
+import { TUI_VALIDATION_ERRORS } from "@taiga-ui/kit";
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +18,20 @@ export const appConfig: ApplicationConfig = {
       provide: TUI_LANGUAGE,
       useValue: of(TUI_FRENCH_LANGUAGE),
     },
+    {
+      provide: TUI_SANITIZER,
+      useClass: NgDompurifySanitizer
+    },
+    {
+      provide: TUI_VALIDATION_ERRORS,
+      useValue: {
+        passwordMismatch: 'Les mots de passe ne correspondent pas',
+        minLength: ({ requiredLength }: { requiredLength: string }) => `Vous devez entrer au moins ${requiredLength} caractères`
+      }
+    },
+    {
+      provide: TUI_LANGUAGE,
+      useValue: of(TUI_FRENCH_LANGUAGE)
+    }
   ]
 };
