ab3b854d4862a57cfe42ff9f78af01cfce615036
Franck <gutmann.franck@outlook.fr>
2024-02-26 09:39:43 +0100

    build: install tui-addon-commerce

--- frontend/src/app/components/product-card/product-card.component.ts
+++ frontend/src/app/components/product-card/product-card.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Product } from '../../models/product';
+import { TuiIslandModule } from '@taiga-ui/kit';
 
 @Component({
     selector: 'app-product-card',
     standalone: true,
     imports: [
         CommonModule,
+        TuiIslandModule
     ],
     templateUrl: './product-card.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
