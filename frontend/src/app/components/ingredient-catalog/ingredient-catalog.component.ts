a3c0c1b85e4e53fa59f673c82b91442adfa92eb4
Franck <gutmann.franck@outlook.fr>
2024-02-27 11:00:00 +0100

    feat(ingredient-catalog): implement search

--- frontend/src/app/components/ingredient-catalog/ingredient-catalog.component.ts
+++ frontend/src/app/components/ingredient-catalog/ingredient-catalog.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { TuiTabsModule } from '@taiga-ui/kit';
-import { TuiScrollbarModule } from '@taiga-ui/core';
+import { TuiInputModule, TuiTabsModule } from '@taiga-ui/kit';
+import { TuiScrollbarModule, TuiTextfieldControllerModule } from '@taiga-ui/core';
 import { TuiLetModule } from '@taiga-ui/cdk';
 import { IngredientsService } from '../../services/ingredients.service';
 import {
@@ -9,6 +9,9 @@ import {
     CdkDropList,
 } from '@angular/cdk/drag-drop';
 import { IngredientTileComponent } from '../ingredient-tile/ingredient-tile.component';
+import { debounceTime, distinctUntilChanged, map, of, switchMap } from 'rxjs';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ingredient } from '../../models/ingredient';
 
 @Component({
     selector: 'app-ingredient-catalog',
@@ -20,14 +23,42 @@ import { IngredientTileComponent } from '../ingredient-tile/ingredient-tile.comp
         TuiLetModule,
         IngredientTileComponent,
         CdkDrag,
-        CdkDropList
+        CdkDropList,
+        TuiInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TuiTextfieldControllerModule
     ],
     templateUrl: './ingredient-catalog.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IngredientCatalogComponent {
 
-    items$ = inject(IngredientsService).getIngredientCategories();
+    search = new FormControl<string>('');
+
+    categories$ = inject(IngredientsService).getIngredientCategories();
+
+    items$ = this.categories$.pipe(
+        map((categories) => {
+            return categories.reduce((acc, category) => {
+                return acc.concat(category.ingredients);
+            }, [] as Ingredient[]);
+        })
+    );
+
+    searchResults$ = this.search.valueChanges.pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((search) => {
+            return this.items$.pipe(
+                map((items) => {
+                    return items.filter((item) => {
+                        return item.name.toLowerCase().includes(search?.toLowerCase() ?? '');
+                    });
+                })
+            );
+        })
+    );
 
     index = 0;
 
